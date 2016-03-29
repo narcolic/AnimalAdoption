c1d82e900ace2811a7fe71ca6130de24ad738e07
narcolic <narcolic832@gmail.com>
2016-03-29 21:36:33 +0300

    databaseservice done

--- services/databaseService.php
+++ services/databaseService.php
@@ -28,4 +28,64 @@ class DatabaseService
             array_push($ex->getMessage(), $this->errors);
         }
     }
+
+    public function getPendingAdoptReq() {
+        $query = "select animalID,name,dateofbirth,description,photo from animal where available = TRUE ";
+        try {
+            $rows = $this->pdo-> query($query);
+            if ($rows && $rows->rowCount() ==1) {
+                $row=$rows->fetch();
+                $animalarray = array($row['animalID'],$row['name'],$row['dateofbirth'],$row['description'],$row['photo']);
+                return $animalarray;
+            }
+            else return null;
+        } catch (PDOException $ex) {
+            array_push($ex->getMessage(), $this->errors);
+        }
+    }
+
+    public function allAdoptReq() {
+        $query = "select animalID,name,dateofbirth,description,photo from animal where animalID = (SELECT animalID from adoptionrequest WHERE approved IS NOT NULL ) ";
+        try {
+            $rows = $this->pdo-> query($query);
+            if ($rows && $rows->rowCount() ==1) {
+                $row=$rows->fetch();
+                $animalarray = array($row['animalID'],$row['name'],$row['dateofbirth'],$row['description'],$row['photo']);
+                return $animalarray;
+            }
+            else return null;
+        } catch (PDOException $ex) {
+            array_push($ex->getMessage(), $this->errors);
+        }
+    }
+
+    public function userPendingAdoptReq($id) {
+        $query = "select animalID,name,dateofbirth,description,photo from animal where animalID = (SELECT animalID from adoptionrequest WHERE userID = '$id' AND approved IS NULL ) ";
+        try {
+            $rows = $this->pdo-> query($query);
+            if ($rows && $rows->rowCount() ==1) {
+                $row=$rows->fetch();
+                $animalarray = array($row['animalID'],$row['name'],$row['dateofbirth'],$row['description'],$row['photo']);
+                return $animalarray;
+            }
+            else return null;
+        } catch (PDOException $ex) {
+            array_push($ex->getMessage(), $this->errors);
+        }
+    }
+
+    public function userOwnedAnimals($id) {
+        $query = "select animalID,name,dateofbirth,description,photo from animal where animalID =(SELECT animalID FROM owns where userID = '$id') ";
+        try {
+            $rows = $this->pdo-> query($query);
+            if ($rows && $rows->rowCount() ==1) {
+                $row=$rows->fetch();
+                $animalarray = array($row['animalID'],$row['name'],$row['dateofbirth'],$row['description'],$row['photo']);
+                return $animalarray;
+            }
+            else return null;
+        } catch (PDOException $ex) {
+            array_push($ex->getMessage(), $this->errors);
+        }
+    }
 }
