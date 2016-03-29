f421b1de19e053af1327c55e277b48905717983e
narcolic <narcolic832@gmail.com>
2016-03-29 12:49:44 +0300

    user-> register Staff->animalmanag/adoptreq

--- views/staff/adoptionRequests.php
+++ views/staff/adoptionRequests.php
@@ -5,3 +5,47 @@
  * Date: 28/3/2016
  * Time: 10:56 μμ
  */
+
+//table of pending adopt requests
+echo "<table>";
+echo "<tr>";
+echo "<th>Animal_ID</th>";
+echo "<th>Name</th>";
+echo "<th>Date_Of_Birth</th>";
+echo "<th>Description</th>";
+echo "<th>Photo</th>";
+echo "</tr>";
+
+$pendquery = "select animalID,name,dateofbirth,description,photo from animal where available = TRUE ";
+
+$rows = $db->query($pendquery);
+//loop through all the returned records and put them into the table cells
+foreach ($rows as $row) {
+    echo "<tr><td >" . $row['animalID'] . "</td><td >" . $row['name'] . "</td><td >" . $row['dateofbirth'];
+    echo "</td><td >" . $row['description'] . "</td><td ><img src=" . $row['photo'] . " alt='Smiley face' height='50' width='50'></td></tr>\n";
+}
+echo "</table>";
+echo "<br/>";
+
+
+//table of all adopt requests
+echo "<table>";
+echo "<tr>";
+echo "<th>Animal_ID</th>";
+echo "<th>Name</th>";
+echo "<th>Date_Of_Birth</th>";
+echo "<th>Description</th>";
+echo "<th>Photo</th>";
+echo "</tr>";
+
+$allquery = "select animalID,name,dateofbirth,description,photo from animal where animalID = (SELECT animalID from adoptionrequest WHERE approved != NULL ) ";
+
+$rows = $db->query($allquery);
+//loop through all the returned records and put them into the table cells
+foreach ($rows as $row) {
+    echo "<tr><td >" . $row['animalID'] . "</td><td >" . $row['name'] . "</td><td >" . $row['dateofbirth'];
+    echo "</td><td >" . $row['description'] . "</td><td ><img src=" . $row['photo'] . " alt='Smiley face' height='50' width='50'></td></tr>\n";
+}
+echo "</table>";
+echo "<br/>";
+
--- views/user/register.php
+++ views/user/register.php
@@ -5,3 +5,45 @@
  * Date: 28/3/2016
  * Time: 10:58 μμ
  */
+
+
+session_start();
+
+
+if (isset($_POST['submitted'])){
+    //create a database connection
+
+    require_once("connectdb.php");
+
+    //echo mysql_ping() ? 'true' : 'false';
+
+    //get and sanitise the inputs, we don't need to do this with the password as we hash it anyway
+    $safe_username = $db->quote($_POST['username']);
+    $hashed_password = md5($_POST['password']);
+
+    //insert the entry into the database
+    $query = "insert into user values ('$safe_username', FALSE, '$hashed_password')";
+
+    $db->exec($query);
+
+    //get the ID
+    $id = $db->lastInsertId();
+
+    //Output success or the errors
+    echo "Congratulations! You are now registered. Your ID is: $id";
+}
+?>
+
+
+
+<html>
+<body>
+<form action = "register.php" method = "post">
+
+    <p>User Name: <input type="text" name="username" size="15" maxlength="20" /></p>
+    <p>Password: <input type="password" name="password" size="15" maxlength="20" /></p>
+    <p><input type="submit" name="submit" value="Submit" /></p>
+    <input type="hidden" name="submitted" value="TRUE" />
+</form>
+</body>
+</html>
